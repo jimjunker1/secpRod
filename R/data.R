00764768b9a0bbf6d208cc97693e513f32a8dcce
jimjunker1 <james.junker1@gmail.com>
2023-10-05 12:57:42 -0500

    added data object 'wbtData

---
+++ R/data.R
@@ -0,0 +1,19 @@
+#'
+#'
+#'
+#'
+#' Sample data for community secondary production analysis
+#'
+#' A collection of community macroinvertebrate data for estimating secondary production
+#'
+#' @format ## `wbtData`
+#' A list of two objects. One list of 32 data frames for each taxonomic entity and the second a data frame with 32 rows and 13 columns:
+#' \describe{
+#'   \item{taxonID}{the taxonomic identifier}
+#'   \item{repID}{the replicate identifier}
+#'   \item{dateID}{the date identifier as.Date format}
+#'   \item{lengthClass}{the numeric (coercible) description of individual length}
+#'   \item{n_m2}{the count or density of individuals from a lengthClass}
+#' }
+#' @source <https://doi.org/10.4319/lo.2014.59.2.0507>
+"wbtData"
