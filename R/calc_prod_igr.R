17d2d356719173f4d7179c9f08a2fd1ddc0a4c94
Jim Junker <james.junker1@gmail.com>
2025-06-03 17:58:51 -0500

    repo cleaning for pushing new stuff on working that hasn't integrated into main yet.

--- R/calc_prod_igr.R
+++ R/calc_prod_igr.R
@@ -7,7 +7,7 @@
 #' @param taxaInfo The taxa info data.frame
 #' @param bootNum integer. The number of bootstrap samples to produce.
 #' @param dateDf data frame of date information with external predictors for each month. There should be a column name identical to all variables in the growth equation found in taxaInfo data.frame.
-#' @param taxaSummary string of 'short', 'full', or 'none'. What type of summary information should be returned.
+#' @param taxaSummary string of \code{'short'}, \code{'full'}, or \code{'none'}. What type of summary information should be returned.
 #' @param wrap logical. Should the dates wrap to create a full year?
 #' @param massValue string. What is the mass value and units of the production
 #' @param massLabel string. What label should the output units be. It is possible this will default to 'massValue' in the future.
@@ -40,7 +40,60 @@ calc_prod_igr <- function(taxaSampleListMass= NULL,
   # - parse all the unique variables
   # - confirm if g_d is present, check for other contributed variables e.g., massValue, lengthClass,etc.
   # - check for novel variables, e.g., temperature, OM, etc. and their presence in envData
+  # convert the length-to-mass formula to a formula
+  massFormula <- as.formula(paste0(gsub(" ", "", taxaSubInfo$massForm)))
+  # if(!as.formula(massFormula)) stop("The length-to-mass formula, 'massForm', cannot be coercied into a formula class.")
+  # get the mass units from the LHS of massFormula
+  massUnits <- formula.tools::lhs(massFormula)
+  # get the variables from the RHS of massFormula
+  massRHS <- formula.tools::rhs(massFormula)
+  # detect if polynomial expression exists
+  caret_present <- any(grepl("\\^", massRHS, ignore.case = TRUE))
+  # if (caret_present) warning("R cannot parse functions with '^'.)
+  if (!any(grepl("lengthClass", massRHS, ignore.case = FALSE))) stop("`lengthClass` is not present in the length-mass (L-M) formula. This variable must be present to convert from length to mass with user-defined L-M function.")
+  ## The code below is a work in progress to parse the formula when `^` is present. This is a feature for later. I will likely remove this note and code and add it to a development branch in the near future.
+  # if(caret_present){
+  #   # deparse to convert 'call' class to character
+  #   charRHS = deparse(massRHS)
+  #   # split on punctuation to extract just variable names
+  #   allVars = sapply(unlist(strsplit(charRHS, "[[:punct:]]")), trimws)
+  #   # remove any blank spaces from the vector
+  #   allVars = otherVars[grepl( "\\w", otherVars)]
+  #   # detect how many variables do not have columns
+  #   if(sum(massRHS %ni% colnames(taxaSubInfo)) > 1) stop("There are multiple undefined terms in the length-to-mass equation. There should only be a one (1) corresponding to lengthClass.")
+  #   lengthVar = allVars[allVars %ni% colnames(taxaSubInfo)]
+  #   names(lengthVar) <- 'lengthClass'
+  #
+  #   powerVars = unlist(strsplit(gsub("^.+(\\w\\^\\w).+$","\\1", charRHS),"\\^"))
+  #   collapsePowerVars = paste(powerVars, collapse = "")
+  #   removeCaret = gsub("\\^"," ", charRHS)
+  #   replaceCaret = gsub(colPowerVars, powerVars, removeCaret)
+  #
+  #
+  #   varRHS = rhs.vars(newRHS)
+  # }
+  # confirm all variables are present in the taxaInfo sheet
+  # deparse to convert 'call' class to character
+  charRHS <- deparse(massRHS)
+  # split on punctuation to extract just variable names
+  allVars <- sapply(unlist(strsplit(charRHS, "[[:punct:]]")), trimws)
+  # remove any blank spaces from the vector
+  allVars <- allVars[grepl("\\w", allVars)]
+  if (!any(grepl("lengthClass", allVars, ignore.case = FALSE))) stop("lengthClass must be a term in the length-to-mass equation.")
+  # remove "poly" for polynomials
+  allVars <- allVars[allVars %ni% "lengthClass"]
+  # detect how many variables do not have columns
+  if (sum(allVars %ni% names(taxaSubInfo)) > 1) stop("There are multiple undefined terms in the length-to-mass equation. There should only be a one (1) corresponding to lengthClass.")
 
+  # fill in the named variables L-M equation with their taxon-specific values
+  ## create a named list of all variables
+  allVars <- setNames(allVars, nm = taxaSubInfo[, names(taxaSubInfo) %in% allVars])
+  ## parse and replace the RHS of the L-M function call
+  newRHS = sapply(charRHS, function(x) replace_formula_terms(x, termList = allVars))
+  ## Recombine the LHS & RHS to a new function
+  newMassFormula = as.formula(paste0(massUnits,"~",newRHS))
+  massUnits = lhs(newMassFormula)
+  massRHS = rhs(newMassFormula)
 
   ## end tests ##
   speciesName = unique(taxaSampleListMass$taxonID)
